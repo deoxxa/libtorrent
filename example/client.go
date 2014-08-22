6b14619ec7351b771053d18cf1c2eacb56602583
deoxxa <deoxxa@fknsrs.biz>
2014-08-22 19:39:54 +1000

    moved import

--- example/client.go
+++ example/client.go
@@ -7,11 +7,11 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/facebookgo/stackerr"
 	"github.com/deoxxa/libtorrent"
 	"github.com/deoxxa/libtorrent/store/tree"
 	"github.com/deoxxa/libtorrent/tracker/http"
 	"github.com/deoxxa/libtorrent/tracker/udp"
+	"github.com/facebookgo/stackerr"
 	"github.com/wsxiaoys/terminal/color"
 )
 
